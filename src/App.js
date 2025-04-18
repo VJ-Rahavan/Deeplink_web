4035a6e7e7a590f6055e9cf19e8b832c192b36db
Vijayarahavan <vijayarahavan176@gmail.com>
2025-04-18 23:26:46 +0530

    feat: Dl setup

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button
+        style={{
+          backgroundColor: 'blue',
+          color: 'white',
+          padding: '10px 30px',
+          borderRadius: '5px',
+        }}
+        onClick={() => {
+          window.location.href = 'myapp://profile/123';
+        }}
+      >
+        Helllo
+      </button>
     </div>
   );
 }
